deb1f03e42ed5f20c08ff33812b21250e4c280e0
Nilton Volpato <nilton.volpato@gmail.com>
2017-03-08 14:53:30 -0800

    new prompt and fileutil alias

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,17 +1,43 @@
-#set -g __prompt_sep '>'
-set -g __prompt_sep \uE0B1
+set -g __prompt_style alternate
 
 function fish_prompt --description 'Write out the prompt'
-  set -l suffix
-  switch $USER
-    case root toor
-      set_color $fish_color_cwd_root
-    case '*'
-      set_color $fish_color_cwd
+  switch $__prompt_style
+    case default
+      switch $USER
+        case root toor
+          set_color $fish_color_cwd_root
+        case '*'
+          set_color $fish_color_cwd
+      end
+      echo -n -s "$USER" ' '
+      echo -n -s (set_color blue) (prompt_pwd) ' '
+      echo -n -s (set_color --bold blue) '$ ' (set_color normal)
+    case alternate
+      echo -n -s (set_color --bold blue) '$ ' (set_color normal)
   end
-
-  echo -n -s "$USER" ' ' (set_color normal) "$__prompt_sep "
 end
+
+function fish_right_prompt
+  switch $__prompt_style
+    case default
+    case alternate
+      set_color $fish_color_autosuggestion[1]
+      echo -n $__right_prompt_sep' '
+      prompt_pwd
+      set_color normal
+  end
+end
+
+function toggle_prompt_style
+  switch $__prompt_style
+    case default
+      set __prompt_style alternate
+    case alternate
+      set __prompt_style default
+  end
+  commandline -f repaint
+end
+bind \cx toggle_prompt_style
 
 function maybe_execute
   set -l CMDLINE (commandline)
@@ -25,11 +51,17 @@ function command_summary --on-event fish_postexec
   # Save the last status for later, otherwise any command will override it.
   set -l last_status $status
 
-  [ -z "$argv" ]; and return
-  [ "$CMD_DURATION" -lt 100 ]; and return
+  [ -z "$argv" ]; and return  # Do not output anything if there was no command.
 
-  set_color $fish_color_autosuggestion[1]
-  echo -n Elapsed: (humanize_duration $CMD_DURATION)
+  if [ $last_status -ne 0 ]
+    set_color $fish_color_error[1]
+    echo Exit status: $last_status
+  end
+
+  if [ "$CMD_DURATION" -ge 100 ]
+    set_color $fish_color_autosuggestion[1]
+    echo Time elapsed: (humanize_duration $CMD_DURATION)
+  end
+
   set_color normal
-  echo
 end
